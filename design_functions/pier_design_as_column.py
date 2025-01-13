c867a501f9a22952d845611d61a836ca88f7e0c6
zemoustafa <119140768+zemoustafa@users.noreply.github.com>
2025-01-13 20:55:33 +0800

    add to pier design

--- design_functions/pier_design_as_column.py
+++ design_functions/pier_design_as_column.py
@@ -16,20 +16,25 @@ from concreteproperties import (
 from concreteproperties.design_codes import AS3600
 from concreteproperties.results import MomentInteractionResults
 
-# input parameters
-fc = 50
-d = 1000
-b = 350
-h = 4200
-v_bar_dia = 24
-h_bar_dia = 16
-cover = 30
-v_bar_cts = 200
-h_bar_cts = 200
-n_star = 5000
-m_star = 25000
-v_star = 5500
-mu_sp = 2.6
+
+def __round_up_to_even(f):
+    return math.ceil(f / 2.) * 2
+
+def __column_buckling_load(d, v_bar_dia, h_bar_dia, cover, p_g, p_q):
+
+    # Column buckling load - Cl 10.4.4
+    k_u = 0.545
+    phi = 0.65
+    m_c = 0
+    d_o = d - cover - h_bar_dia - v_bar_dia/2
+    alpha_s = 0.6
+    E_c = 40000
+    beta_d = p_g / (p_g + p_q)
+    n_c = (math.pi**2 * h**2) * ((182 * d_o * m_c) / (1 + beta_d))
+    pass
+
+def __moment_magnification_factor():
+    pass
 
 def moment_interaction_design(fc, cover, d, b, v_bar_dia, v_bar_cts, h_bar_dia, n_star, m_star):
     # calculations
@@ -88,7 +93,7 @@ def moment_interaction_design(fc, cover, d, b, v_bar_dia, v_bar_cts, h_bar_dia, 
     return pass_fail
 
 
-def calculate_effective_shear_depth(d, cover, v_bar_area, v_bar_cts, h_bar_dia):
+def __calculate_effective_shear_depth(d, cover, v_bar_area, v_bar_cts, h_bar_dia):
     '''
     Determine effective shear depth of deep beam section
 
@@ -121,11 +126,13 @@ def calculate_effective_shear_depth(d, cover, v_bar_area, v_bar_cts, h_bar_dia):
 
     dsy = sum(Ast[i] * d_si[i] for i in range(len(Ast))) / sum(Ast)
     dvy = max(0.72*d, 0.9*dsy)
-    return dvy
+    ast_total = round( sum(Ast[i] for i in range(len(Ast))), 0 ) # ast within tensile region
+    print('dsy = ' + str(dsy) + ' mm')
+    return dvy, ast_total
 
 def column_shear(fc, cover, d, b, h, v_bar_dia, v_bar_cts, h_bar_dia, h_bar_cts):
     '''
-    Determines shear capacity of column based on beam shear to Section 8
+    Determines concrete contribution to shear capacity of column based on beam shear to Section 8
 
     Parameters:
     fc (float): concrete strength
@@ -138,29 +145,55 @@ def column_shear(fc, cover, d, b, h, v_bar_dia, v_bar_cts, h_bar_dia, h_bar_cts)
     h_bar_cts (float): spacing of horizontal bars
 
     '''
-
+    # calculate reo area and reo rate in section
     fsy = 500 # MPa
     h_bar_area = math.pi * h_bar_dia**2 / 4 # mm2
     v_bar_area = math.pi * v_bar_dia**2 / 4 # mm2
-    n_bars_v = round(2 * ( d - 2 * cover - 2 * h_bar_dia ) / v_bar_cts, 0)
-    n_bars_h = round(2 * ( h - 2 * cover ) / h_bar_cts, 0)
+    a_ct = d * b / 2 # mm2 - area of concrete in tensile region
+    n_bars_v = __round_up_to_even(2 * ( d - 2 * cover - 2 * h_bar_dia ) / v_bar_cts) # no. vertical bars in tensile region
+    n_bars_h = round(2 * ( h - 2 * cover ) / h_bar_cts, 0) # no. horizontal bars in section
+
+    rho = ( n_bars_v * v_bar_area / (d * b) ) * 100 # reinforcement rate
 
     # Concrete contribution to shear cl 8.2.4
-    strain_x = 0
-    # Simplified method for kv and theta v cl 8.2.4.3
-    theta_v = (29 + 7000 * strain_x)
-    A_sv_min = h_bar_cts * 0.08 * math.sqrt(fc) * b / fsy
-    A_sv = h_bar_area * n_bars_h
+    dvy, ast_total = __calculate_effective_shear_depth(d, cover, v_bar_area, v_bar_cts, h_bar_dia)
+    dvs = d / 2 - dvy
+    strain_x = ( (m_star*1000000)/dvy + (v_star*1000) ) / ( 2*( dvs/ dvy * 200000 * ast_total))
 
+    # Simple method for kv and theta v
+    theta_v = 36 # angle of inclination of concrete strut
+    theta_v_rads = math.radians(theta_v)
+    A_sv_min = h_bar_cts * 0.08 * math.sqrt(fc) * b / fsy  # Minimum transverse shear reinforcement - Cl 8.2.1.7
+    A_sv = h_bar_area * n_bars_h
     kv = 200/(100 + 1.3*d) if A_sv/h_bar_cts < A_sv_min/h_bar_cts else 0.15
-    dvy = calculate_effective_shear_depth(d, cover, v_bar_area, v_bar_cts, h_bar_dia)
-    bvy = b
-    v_uc = round(kv * bvy * dvy * max(math.sqrt(fc), 8) / 1000, 0)
-    v_us = round(h_bar_area * fsy * dvy / h_bar_cts * (math.cos(theta_v)/math.sin(theta_v)) / 1000, 0)
+
+    # concrete contribution to shear strength
+    v_uc = round(kv * b * dvy * min(math.sqrt(fc), 8) / 1000, 0)
+
+    # Transverse shear and torsion reinforcement contribution - Cl 8.2.5
+    v_us = ((2 * h_bar_area) * fsy * dvy / h_bar_cts) * (1/math.tan(theta_v_rads)) / 1000
+
+    # return values
     return v_uc, v_us
 
+# input parameters
+fc = 50
+d = 3000
+b = 300
+h = 4200
+v_bar_dia = 24
+h_bar_dia = 16
+cover = 30
+v_bar_cts = 200
+h_bar_cts = 200
+n_star = 5000
+m_star = 200
+v_star = 500
+mu_sp = 2.6
 
+v_uc, v_us = column_shear(fc, cover, d, b, h, v_bar_dia, v_bar_cts, h_bar_dia, h_bar_cts)
 
-v_uc, v_us = column_shear(fc, cover, d, b, h, v_bar_dia, v_bar_cts, h_bar_dia, h_bar_cts, mu_sp, v_star, m_star, n_star)
-print('Vuc = ' + str(v_uc))
-print('Vus = ' + str(v_us))
+print('D =' + str(d) + ' mm')
+print('B =' + str(b) + ' mm')
+print('Vuc = ' + str(v_uc) + ' kN')
+print('Vus = ' + str(v_us) + ' kN')
