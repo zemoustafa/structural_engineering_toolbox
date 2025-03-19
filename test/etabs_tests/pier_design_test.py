7d209272495ef83abe33acb19eca34ed00cced9b
zemoustafa <119140768+zemoustafa@users.noreply.github.com>
2025-03-19 15:22:21 +0800

    update comments

--- test/etabs_tests/pier_design_test.py
+++ test/etabs_tests/pier_design_test.py
@@ -3,13 +3,19 @@ sys.path.append('C:\_Github\structural_engineering_toolbox')
 from etabs_tools import etabs_api, etabs_design_v1
 from design_reports import design_reports
 
+# Button 1 - connect to API and grab load case names
+# Call function etabs_api.get_load_cases_and_combinations() to get load combination names as a list of dic
+
 etabs_api = etabs_api.etabs_api()
 
-eq_env_1 = '(88) RS ULS ENV' # earthquake envelope unfactored EQ for moment design
-eq_env_2 = '(88) RS ULS ENV SHEAR' # earthquake envelope factored EQ for amplified shear design
-wind_env = '(88) WIND ULS ENV' # wind envelope
+# Once we have the load case names, let the user select the relevant case for each envelope from a list or dropdown
+# Envelopes need to be Earthquake Flexure, Earthquake Shear, and Wind
+eq_env_1 = '(88) RS ULS ENV' # Earthquake Flexure -> earthquake envelope unfactored EQ for moment design
+eq_env_2 = '(88) RS ULS ENV SHEAR' # Earthquake Shear -> earthquake envelope factored EQ for amplified shear design
+wind_env = '(88) WIND ULS ENV' # Wind -> wind envelope
 
-
+# Once load cases are selected:
+# Button 2 - grab pier properties and forces - and run the analysis
 try:
     piers = etabs_api.get_piers(load_cases=[eq_env_1, eq_env_2, wind_env])
 except Exception as e:
@@ -27,4 +33,13 @@ designed_piers_df = etabs_design_v1.design_all_piers(
     design_boundary_element=True
 )
 
+# Once it has been run, display the summary table of pier design results
+
+# Button 3 - generate report
 design_reports.dataframe_to_xlsx(designed_piers_df)
+
+# Next steps:
+# View indiviudal pier design diagrams when clicking on a row in the summary table
+# Add summary section, eg number of walls, number of walls pass/fdailed, etc
+# Locations of walls on plan?
+# Power BI dashboard?
